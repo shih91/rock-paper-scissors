a0531c4f113c138b7577a64e09a02ce482f4a4d8
Shih Chiang Liu <shih91@gmail.com>
2022-11-06 09:24:24 +0800

    added template for results

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,6 +47,37 @@ export default function Home() {
 					</div>
 				))}
 			</div>
+
+			<div className="flex p-4">
+				<div className="border-2 border-dotted w-60 h-60 m-8">
+					<p className="text-cyan-900 font-bold">You</p>
+					<p className="justify-center">Score</p>
+					<div className="m-8">
+						<Image
+							src="https://goodday451999.github.io/Rock-Paper-Scissors-Neo/images/scissors.png"
+							alt="it's a scissors"
+							width={150}
+							height={150}
+							onClick={() => matchStart()}
+							className={btn}
+						/>
+					</div>
+				</div>
+				<div className="border-2 border-dotted w-60 h-60 m-8">
+					<p className="text-right text-cyan-900 font-bold">Computer</p>
+					<p className="text-right justify-center">Score</p>
+					<div className="m-8">
+						<Image
+							src="https://goodday451999.github.io/Rock-Paper-Scissors-Neo/images/scissors.png"
+							alt="it's a scissors"
+							width={150}
+							height={150}
+							onClick={() => matchStart()}
+							className={btn}
+						/>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }

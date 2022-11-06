01fd5876c9f60dd4728deb367144b3a9440ade94
Shih Chiang Liu <shih91@gmail.com>
2022-11-06 09:13:23 +0800

    use object.entries to map elements instead of hardcoding

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,21 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+const elements = {
+	1: {
+		url: "https://goodday451999.github.io/Rock-Paper-Scissors-Neo/images/stone.png",
+		alt: "it's a rock",
+	},
+	2: {
+		url: "https://goodday451999.github.io/Rock-Paper-Scissors-Neo/images/paper.png",
+		alt: "it's a paper",
+	},
+	3: {
+		url: "https://goodday451999.github.io/Rock-Paper-Scissors-Neo/images/scissors.png",
+		alt: "it's a scissors",
+	},
+};
+
 const btn =
 	"hover:cursor-pointer hover:border-dotted hover:border-violet-500 border-4 rounded-full border-1";
 
@@ -19,36 +34,18 @@ export default function Home() {
 				Rock Paper Scissors!
 			</h1>
 			<div className="flex h-56 justify-item-center">
-				<div className="m-8">
-					<Image
-						src="https://goodday451999.github.io/Rock-Paper-Scissors-Neo/images/stone.png"
-						alt="it's a rock"
-						width={150}
-						height={150}
-						onClick={() => matchStart()}
-						className={btn}
-					/>
-				</div>
-				<div className="m-8">
-					<Image
-						src="https://goodday451999.github.io/Rock-Paper-Scissors-Neo/images/paper.png"
-						alt="it's a paper"
-						width={150}
-						height={150}
-						onClick={() => matchStart()}
-						className={btn}
-					/>
-				</div>
-				<div className="m-8">
-					<Image
-						src="https://goodday451999.github.io/Rock-Paper-Scissors-Neo/images/scissors.png"
-						alt="it's a scissors"
-						width={150}
-						height={150}
-						onClick={() => matchStart()}
-						className={btn}
-					/>
-				</div>
+				{Object.entries(elements).map(([key, element], i) => (
+					<div className="m-8">
+						<Image
+							src={element.url}
+							alt={element.alt}
+							width={150}
+							height={150}
+							onClick={() => matchStart()}
+							className={btn}
+						/>
+					</div>
+				))}
 			</div>
 		</div>
 	);
